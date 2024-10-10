ab5802033f7ba771a5692a648d1c8aa7ecd8dbe6
AD\krell.chiparausha <krell.chiparausha@wirk.co.za>
2024-10-10 08:53:42 +0200

    built staic

--- feedback_app/feedback_app/settings.py
+++ feedback_app/feedback_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,8 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'  # This is usually already defined
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
