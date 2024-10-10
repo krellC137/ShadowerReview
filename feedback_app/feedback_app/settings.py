b946c4d25754bbf92e7a1c258d6839ca983dc4a4
AD\krell.chiparausha <krell.chiparausha@wirk.co.za>
2024-10-10 12:51:47 +0200

    changed database settings

--- feedback_app/feedback_app/settings.py
+++ feedback_app/feedback_app/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'feedback_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
