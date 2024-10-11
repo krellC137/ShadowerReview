3ab1d38fc812b44b84ae5276c76b7998025313af
AD\krell.chiparausha <krell.chiparausha@wirk.co.za>
2024-10-11 08:56:54 +0200

    trying to fix staic files

--- feedback_app/feedback_app/settings.py
+++ feedback_app/feedback_app/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure this directory exists on your server
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # During production, also ensure you have:
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
